0ff59a98b1a3fe4b39f60a1458b9568d8c40545f
Meister <s52150@edu.campus02.at>
2022-09-23 17:02:35 +0200

    First

---
+++ MyFirstClass.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+
+
+public class MyFirstClass {
+
+
+
+    public static void main(String[] args)
+        {
+
+            //erste Ausgabe
+            System.out.println("Hello world");
+
+            // Sollte die Werte bei der Eingabe mit ausgeben
+            System.out.println(Arrays.toString(args));
+
+        }
+
+
+}
